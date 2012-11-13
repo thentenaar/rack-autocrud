98187397575d1e3bd3d10738bf61b26bed9e8ef2
Tim Hentenaar <tim@hentenaar.com>
2012-11-13 16:46:21 +0100

    Retab

--- lib/rack/autocrud.rb
+++ lib/rack/autocrud.rb
@@ -15,160 +15,160 @@ require 'sinatra/base'
 require 'json'
 
 module Rack
-	class AutoCRUD
-		def initialize(app,options={})
-			@app                = app
-			@model_namespace    = options[:model_namespace]
-			@endpoint_namespace = options[:endpoint_namespace]
-			@endpoint_mod       = nil
-		end
+  class AutoCRUD
+    def initialize(app,options={})
+      @app                = app
+      @model_namespace    = options[:model_namespace]
+      @endpoint_namespace = options[:endpoint_namespace]
+      @endpoint_mod       = nil
+    end
 
-		def call(env)
-			dup._call(env) # For thread safety...
-		end
+    def call(env)
+      dup._call(env) # For thread safety...
+    end
 
-		def _call(env)
-			model_klass        = nil
-			endpoint_klass     = nil
-			verb,endpoint,*uri = env['REQUEST_URI'].split('/')
-			verb               = env['REQUEST_METHOD'].downcase
+    def _call(env)
+      model_klass        = nil
+      endpoint_klass     = nil
+      verb,endpoint,*uri = env['REQUEST_URI'].split('/')
+      verb               = env['REQUEST_METHOD'].downcase
 
-			# Enumerate through all defined classes, checking for the model / endpoint
-			ObjectSpace.each_object(Class) { |klass|
-				model_klass    = klass if String(klass.name).downcase == String(@model_namespace    + '::' + endpoint).downcase
-				endpoint_klass = klass if String(klass.name).downcase == String(@endpoint_namespace + '::' + endpoint).downcase
-			}
+      # Enumerate through all defined classes, checking for the model / endpoint
+      ObjectSpace.each_object(Class) { |klass|
+        model_klass    = klass if String(klass.name).downcase == String(@model_namespace    + '::' + endpoint).downcase
+        endpoint_klass = klass if String(klass.name).downcase == String(@endpoint_namespace + '::' + endpoint).downcase
+      }
 
-			# Lazily locate the endpoint namespace module (if we haven't already)
-			if endpoint_klass.nil? && @endpoint_mod.nil?
-				ObjectSpace.each_object(Module) { |klass|
-					@endpoint_mod = klass if String(klass.name).downcase == @endpoint_namespace.downcase 
-				}
-			end
+      # Lazily locate the endpoint namespace module (if we haven't already)
+      if endpoint_klass.nil? && @endpoint_mod.nil?
+        ObjectSpace.each_object(Module) { |klass|
+          @endpoint_mod = klass if String(klass.name).downcase == @endpoint_namespace.downcase 
+        }
+      end
 
-			# Now, if we've got something, do our magic.
-			if !model_klass.nil?
-				# If we don't have an endpoint class, make one
-				if endpoint_klass.nil?
-					endpoint_klass = Class.new(Sinatra::Base)
-					@endpoint_mod.const_set(endpoint.capitalize,endpoint_klass)
-				end
+      # Now, if we've got something, do our magic.
+      if !model_klass.nil?
+        # If we don't have an endpoint class, make one
+        if endpoint_klass.nil?
+          endpoint_klass = Class.new(Sinatra::Base)
+          @endpoint_mod.const_set(endpoint.capitalize,endpoint_klass)
+        end
 
-				# Patch in the routes
-				endpoint_klass.class_exec(model_klass,endpoint,env) { |model,endpoint,env|
-					def set_request_body(new_body,content_type='text/json')
-						env['rack.input']     = StringIO.new(new_body)
-						env['CONTENT_LENGTH'] = new_body.length
-						env['CONTENT_TYPE']   = content_type
-						return nil
-					end
+        # Patch in the routes
+        endpoint_klass.class_exec(model_klass,endpoint,env) { |model,endpoint,env|
+          def set_request_body(new_body,content_type='text/json')
+            env['rack.input']     = StringIO.new(new_body)
+            env['CONTENT_LENGTH'] = new_body.length
+            env['CONTENT_TYPE']   = content_type
+            return nil
+          end
 
-					get '/' do
-						halt [403, '{ "error": "Access Denied" }']
-					end
+          get '/' do
+            halt [403, '{ "error": "Access Denied" }']
+          end
 
-					post '/' do
-						# Call the pre-create hook
-						if self.respond_to?(:pre_create)
-							ret = pre_create(env,request)
-							return ret unless ret.nil?
-						end
+          post '/' do
+            # Call the pre-create hook
+            if self.respond_to?(:pre_create)
+              ret = pre_create(env,request)
+              return ret unless ret.nil?
+            end
 
-						# Rewind the body
-						request.body.rewind if request.body.respond_to?(:rewind)
+            # Rewind the body
+            request.body.rewind if request.body.respond_to?(:rewind)
 
-						# Attempt to create the model object
-						obj = nil
-						begin
-							obj = model.create(JSON.parse(request.body.read))
-							halt [402, '{ "error": "Failed to save ' + endpoint + '" }'] unless obj && obj.saved?
-						rescue JSON::ParserError
-							halt [ 400, { 'error' => 'Invalid JSON in request body.', 'details' => $! }.to_json ]
-						end
+            # Attempt to create the model object
+            obj = nil
+            begin
+              obj = model.create(JSON.parse(request.body.read))
+              halt [402, '{ "error": "Failed to save ' + endpoint + '" }'] unless obj && obj.saved?
+            rescue JSON::ParserError
+              halt [ 400, { 'error' => 'Invalid JSON in request body.', 'details' => $! }.to_json ]
+            end
 
-						# Call the post-create hook
-						if self.respond_to?(:post_create)
-							ret = post_create(env,request,obj)
-							return ret unless ret.nil?
-						end
+            # Call the post-create hook
+            if self.respond_to?(:post_create)
+              ret = post_create(env,request,obj)
+              return ret unless ret.nil?
+            end
 
-						[ 201, { 'id' => obj.id.to_i }.to_json ]
-					end
+            [ 201, { 'id' => obj.id.to_i }.to_json ]
+          end
 
-					get '/:id' do
-						# Call the pre-retrieve hook
-						if self.respond_to?(:pre_retrieve)
-							ret = pre_retrieve(env,request)
-							return ret unless ret.nil?
-						end
+          get '/:id' do
+            # Call the pre-retrieve hook
+            if self.respond_to?(:pre_retrieve)
+              ret = pre_retrieve(env,request)
+              return ret unless ret.nil?
+            end
 
-						obj = model.get(params[:id])
+            obj = model.get(params[:id])
 
-						# Call the post-retrieve hook
-						if self.respond_to?(:post_retrieve)
-							ret = post_retrieve(env,request,obj)
-							return ret unless ret.nil?
-						end
+            # Call the post-retrieve hook
+            if self.respond_to?(:post_retrieve)
+              ret = post_retrieve(env,request,obj)
+              return ret unless ret.nil?
+            end
 
-						obj.to_json
-					end
+            obj.to_json
+          end
 
-					put '/:id' do
-						# Call the pre-update hook
-						if self.respond_to?(:pre_update)
-							ret = pre_update(env,request)
-							return ret unless ret.nil?
-						end
+          put '/:id' do
+            # Call the pre-update hook
+            if self.respond_to?(:pre_update)
+              ret = pre_update(env,request)
+              return ret unless ret.nil?
+            end
 
-						# Rewind the body
-						request.body.rewind if request.body.respond_to?(:rewind)
+            # Rewind the body
+            request.body.rewind if request.body.respond_to?(:rewind)
 
-						# Attempt to update the model
-						begin
-							saved = model.update(JSON.parse(request.body.read))
-							halt [403, 'Access Denied'] unless saved
-						rescue JSON::ParserError
-							halt [ 400, { 'error' => 'Invalid JSON in request body.', 'details' => $! }.to_json ]
-						end
+            # Attempt to update the model
+            begin
+              saved = model.update(JSON.parse(request.body.read))
+              halt [403, 'Access Denied'] unless saved
+            rescue JSON::ParserError
+              halt [ 400, { 'error' => 'Invalid JSON in request body.', 'details' => $! }.to_json ]
+            end
 
-						# Call the post-update hook
-						if self.respond_to?(:post_update)
-							ret = post_update(env,request)
-							return ret unless ret.nil?
-						end
+            # Call the post-update hook
+            if self.respond_to?(:post_update)
+              ret = post_update(env,request)
+              return ret unless ret.nil?
+            end
 
-						[ 201, '{ "status": "ok" }' ]
-					end
+            [ 201, '{ "status": "ok" }' ]
+          end
 
-					delete '/:id' do
-						# Call the pre-destroy hook
-						if self.respond_to?(:pre_destroy)
-							ret = pre_destroy(env,request)
-							return ret unless ret.nil?
-						end
+          delete '/:id' do
+            # Call the pre-destroy hook
+            if self.respond_to?(:pre_destroy)
+              ret = pre_destroy(env,request)
+              return ret unless ret.nil?
+            end
 
-						obj = model.get(params[:id])
-						obj.destroy if obj
+            obj = model.get(params[:id])
+            obj.destroy if obj
 
-						# Call the post-destroy hook
-						if self.respond_to?(:post_destroy)
-							ret = post_destroy(env,request,obj)
-							return ret unless ret.nil?
-						end
+            # Call the post-destroy hook
+            if self.respond_to?(:post_destroy)
+              ret = post_destroy(env,request,obj)
+              return ret unless ret.nil?
+            end
 
-						[ 204 ]
-					end
-				}
+            [ 204 ]
+          end
+        }
 
-				# Now, call the endpoint class (assuming it will return a response)
-				env['PATH_INFO'] = '/' + uri.join('/')
-				return endpoint_klass.call(env)
-			end
+        # Now, call the endpoint class (assuming it will return a response)
+        env['PATH_INFO'] = '/' + uri.join('/')
+        return endpoint_klass.call(env)
+      end
 
-			# Otherwise, pass the request down the chain...
-			@app.call(env)
-		end
-	end
+      # Otherwise, pass the request down the chain...
+      @app.call(env)
+    end
+  end
 end
 
-# vi:set ts=2:
+# vi:set ts=2 sw=2 expandtab sta:
--- rack-autocrud.gemspec
+++ rack-autocrud.gemspec
@@ -1,28 +1,29 @@
 Gem::Specification.new do |gem|
-	gem.name        = 'rack-autocrud'
-	gem.version     = '0.1.5'
-	gem.author      = 'Tim Hentenaar'
-	gem.email       = 'tim.hentenaar@gmail.com'
-	gem.homepage    = 'https://github.com/thentenaar/rack-autocrud'
-	gem.summary     = 'Rack middleware that automagically handles basic CRUD operations'
-	gem.description = <<__XXX__
-	Rack middleware that works with Sinatra and DataMapper to dynamically
-	create CRUD endpoints and routes based on models. It ain't perfect, but
-	it works.
+  gem.name        = 'rack-autocrud'
+  gem.version     = '0.1.5'
+  gem.author      = 'Tim Hentenaar'
+  gem.email       = 'tim.hentenaar@gmail.com'
+  gem.homepage    = 'https://github.com/thentenaar/rack-autocrud'
+  gem.summary     = 'Rack middleware that automagically handles basic CRUD operations'
+  gem.description = <<__XXX__
+  Rack middleware that works with Sinatra and DataMapper to dynamically
+  create CRUD endpoints and routes based on models. It ain't perfect, but
+  it works.
 
-	These generated CRUD routes are assumed to return a Rack response.
+  These generated CRUD routes are assumed to return a Rack response.
 
-	It's important to note, that you models and endpoints must be in separate
-	modules (read: namespaces).
+  It's important to note, that you models and endpoints must be in separate
+  modules (read: namespaces).
 
-	Input and Response data are formatted as JSON.
+  Input and Response data are formatted as JSON.
 
-	See the README for more info.
+  See the README for more info.
 __XXX__
 
-	gem.files = Dir['lib/**/*','README*', 'LICENSE']
-	gem.add_dependency 'json'
-	gem.add_dependency 'sinatra'
-	gem.add_dependency 'data_mapper'
+  gem.files = Dir['lib/**/*','README*', 'LICENSE']
+  gem.add_dependency 'json'
+  gem.add_dependency 'sinatra'
+  gem.add_dependency 'data_mapper'
 end
-# vi:set ts=2:
+
+# vi:set ts=2 sw=2 expandtab sta:
